f230144ac920c2de6f4ef51e59b5d667ae7497b1
Hb-nirala <nirala.kumar@hiddenbrains.in>
2023-09-01 17:37:24 +0530

    add Splash Screen

--- src/Screens/Splash.js
+++ src/Screens/Splash.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
 
@@ -15,14 +15,43 @@ const Splash = (props) => {
             else {
                 props.navigation.navigate('Login')
             }
-        }, 2000)
+        }, 5000)
     }, [loginReducerData])
 
     return (
-        <View>
-            <Text>Splash</Text>
+        <View style={styles.containerStyle}>
+            <View style={styles.imageContainerStyle}>
+                <Image
+                    style={styles.imageStyle}
+                    resizeMode='cover'
+                    source={require('../utils/assets/Images/Splash_Image.png')}
+                />
+                <Text style={styles.textStyle}>Redux App</Text>
+            </View>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    imageStyle: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+    },
+    containerStyle: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgb(100,56,177)'
+    },
+    imageContainerStyle: {
+        alignItems: 'center'
+    },
+    textStyle: {
+        marginTop: 10,
+        fontSize: 20,
+        color: 'white',
+        fontWeight: 'bold'
 
+    }
+})
 export default Splash

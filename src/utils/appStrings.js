94e4d80c4cc8020f17c4b9887dbbe53e900b5fa2
Hb-nirala <nirala.kumar@hiddenbrains.in>
2023-10-06 00:38:00 +0530

    Update home tab Theming

--- src/utils/appStrings.js
+++ src/utils/appStrings.js
@@ -30,6 +30,12 @@ export const EnjoyTabImages = {
     music_banner:'https://images.pexels.com/photos/167491/pexels-photo-167491.jpeg?auto=compress&cs=tinysrgb&w=1600',
     youtube_videos_bannerimage:require('../../assets/Images/redux_icon.png')
 }
+export const HomeTabImages = {
+    api_home_banner:'https://play-lh.googleusercontent.com/CX9K92_zQCNfaMebBeIY2g5NbnRzYRWRzSNMR-bZzTZD66R82hYA7naI6g9OZ6J4m7g=h500',
+    add_task_home_banner:'https://thumbs.dreamstime.com/b/businesswoman-hand-holding-wooden-cube-block-task-business-word-table-background-list-schedule-concept-212234578.jpg',
+    chat_home_banner:'https://cdn-icons-png.flaticon.com/512/1310/1310062.png',
+    shopping_cart_home_banner:'https://indian-retailer.s3.ap-south-1.amazonaws.com/s3fs-public/2021-05/revenge%20shopping.jpg',
+}
 export const music={
     music:'Music',
     music_backgroundimage:'https://cdn6.aptoide.com/imgs/7/b/1/7b1c49e454d1ed8e9396dc9d283f7108_icon.png'
